366fd070951f1ef902d2b11d3bf40785d599175b
Milan Mitic <62287697+Milan-Mitic@users.noreply.github.com>
2023-06-13 10:24:10 +0200

    Add files via upload

---
+++ CountDuplicatedNumber/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace CountDuplicatedNumber
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = new int[20];
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                Console.WriteLine("Please enter number " + (i + 1));
+                numbers[i] = int.Parse(Console.ReadLine());
+
+                if (numbers[i] > 100)
+                {
+                    break;
+                }
+            }
+            var dict = new Dictionary<int, int>();
+            foreach (var value in numbers)
+            {
+                if (dict.ContainsKey(value)) dict[value]++;
+                else dict[value] = 1;
+            }
+            foreach (var pair in dict)
+                Console.WriteLine("Value {0} occurred {1} times", pair.Key, pair.Value);
+        }
+    }
+}
