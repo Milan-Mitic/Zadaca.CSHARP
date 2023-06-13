810acdb5a3b162ba5544a559aea7449207a8340b
Milan Mitic <62287697+Milan-Mitic@users.noreply.github.com>
2023-06-13 10:27:38 +0200

    Add files via upload

---
+++ SumOfRandomEven/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace SumOfRandomEven
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = new int[100];
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                Console.WriteLine("Press 0 to stop input value ");
+                Console.WriteLine("Enter number " + (i + 1));
+                numbers[i] = int.Parse(Console.ReadLine());
+
+                if (numbers[i] == 0)
+                {
+                    break;
+                }
+            }
+
+            int sum = 0;
+            foreach (int num in numbers)
+            {
+                if (num % 2 == 0)
+                {
+                    sum += num;
+                }
+            }
+            Console.WriteLine("Sum of even numbers is: " + sum);
+        }
+    }
+}
