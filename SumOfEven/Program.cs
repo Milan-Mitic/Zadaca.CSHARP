455a70e8facba8181ef42eb0ce38ff3595370163
Milan Mitic <62287697+Milan-Mitic@users.noreply.github.com>
2023-06-13 10:26:59 +0200

    Add files via upload

---
+++ SumOfEven/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace SumOfEven
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = new int[6];
+
+            Console.WriteLine("Enter 6 numbers:");
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                Console.Write($"Number {i + 1}: ");
+                numbers[i] = int.Parse(Console.ReadLine());
+            }
+
+            int sum = 0;
+            foreach (int num in numbers)
+            {
+                if (num % 2 == 0)
+                {
+                    sum += num;
+                }
+            }
+            Console.WriteLine("Sum of even numbers: " + sum);
+        }
+    }
+}
