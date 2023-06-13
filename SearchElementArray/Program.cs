2b61ee7d3df0dabfbbd1a37691ca3b76db580751
Milan Mitic <62287697+Milan-Mitic@users.noreply.github.com>
2023-06-13 10:25:41 +0200

    Add files via upload

---
+++ SearchElementArray/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace SearchElementArray
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = { 5, 12, 20, 33, 45, 58 };
+
+            Console.WriteLine("Enter number");
+            int searchNumber = Convert.ToInt32(Console.ReadLine());
+
+            bool containsNumber = numbers.Contains(searchNumber);
+
+            if (containsNumber)
+            {
+                Console.WriteLine("The number exist");
+            }
+            else
+            {
+                Console.WriteLine("The number does not exist");
+            }
+        }
+    }
+}
