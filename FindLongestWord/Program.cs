d903102088a8d71f7c20b260cf0432c9b53f1fcd
Milan Mitic <62287697+Milan-Mitic@users.noreply.github.com>
2023-06-13 10:25:01 +0200

    Add files via upload

---
+++ FindLongestWord/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace FindLongestWord
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] msgArray = { "Send data", "Message arr", "Calculate data", "SEDC Code Academy C# basic", "Play" };
+
+            int i = 0;
+            string longString = msgArray[i];
+
+            foreach (string word in msgArray)
+            {
+                if (longString.Length < word.Length)
+                    longString = word;
+            }
+            Console.WriteLine("The longest word: " + longString);
+            Console.WriteLine("Letters count: " + longString.Length);
+        }
+    }
+}
